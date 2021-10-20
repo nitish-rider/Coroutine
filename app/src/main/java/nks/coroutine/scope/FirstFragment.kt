fd9a3f3f7f84ec035cf29a8bf988c80af5b21ac3
nks102000 <nitishks1110@gmail.com>
2021-10-20 23:23:01 +0530

    Job & Cancellation

--- app/src/main/java/nks/coroutine/scope/FirstFragment.kt
+++ app/src/main/java/nks/coroutine/scope/FirstFragment.kt
@@ -8,14 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
-import kotlinx.coroutines.CoroutineName
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 
 class FirstFragment : Fragment() {
-    val scope= CoroutineScope(CoroutineName("MyScope"))//if ve do not define dispatcher it will take DefaultDispatcher
+    private val scope= CoroutineScope(CoroutineName("MyScope"))//if ve do not define dispatcher it will take DefaultDispatcher
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,14 +21,45 @@ class FirstFragment : Fragment() {
         // Inflate the layout for this fragment
         val view=inflater.inflate(R.layout.fragment_first, container, false)
 
-        val job = scope.launch { //launch returns a job object
-            Log.d("Coroutines",this.coroutineContext.toString())
-            launch {                 //nested scope
-                Log.d("Coroutines",this.coroutineContext.toString())
+        val mainJob=scope.launch { //launch returns a job object
+            val job1=launch {
+                while(isActive){
+                    Log.d("Coroutines","Job 1 Running...")
+                }
             }
+            val job2=launch {
+                while (true){
+                    ensureActive()
+                    Log.d("Coroutines","Job 2 Running...")
+                }
+            }
+            val job3=launch {
+                while (true){
+                    delay(50L)
+                    Log.d("Coroutines","Job 2 Running...")
+                }
+            }
+            val job4=launch {
+                while (true){
+                    yield()
+                    Log.d("Coroutines","Job 2 Running...")
+                }
+            }
+            delay(1000L)
+            Log.d("Coroutines","Canceling..")
+            job1.cancel()
+            job1.join() //will wait till the job is completed and only the log the completed message
+            job3.cancelAndJoin()
+            Log.d("CoroutinesC","Job 1 Cancelled..")
         }
 
+        runBlocking {
+            delay(2000L)
+            Log.d("Coroutines","Canceling..")
+            mainJob.cancelAndJoin()
+            Log.d("CoroutinesC","MainJob Cancelled..")
 
+        }
 
         view.findViewById<TextView>(R.id.first_text).setOnClickListener {
             findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
