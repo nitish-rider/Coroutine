43c56c973375bfa48c4b0907329a92793b2d757a
nks102000 <nitishks1110@gmail.com>
2021-10-20 23:34:49 +0530

    async & await

--- app/src/main/java/nks/coroutine/scope/FirstFragment.kt
+++ app/src/main/java/nks/coroutine/scope/FirstFragment.kt
@@ -12,7 +12,6 @@ import kotlinx.coroutines.*
 
 
 class FirstFragment : Fragment() {
-    private val scope= CoroutineScope(CoroutineName("MyScope"))//if ve do not define dispatcher it will take DefaultDispatcher
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,46 +19,6 @@ class FirstFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view=inflater.inflate(R.layout.fragment_first, container, false)
-
-        val mainJob=scope.launch { //launch returns a job object
-            val job1=launch {
-                while(isActive){
-                    Log.d("Coroutines","Job 1 Running...")
-                }
-            }
-            val job2=launch {
-                while (true){
-                    ensureActive()
-                    Log.d("Coroutines","Job 2 Running...")
-                }
-            }
-            val job3=launch {
-                while (true){
-                    delay(50L)
-                    Log.d("Coroutines","Job 2 Running...")
-                }
-            }
-            val job4=launch {
-                while (true){
-                    yield()
-                    Log.d("Coroutines","Job 2 Running...")
-                }
-            }
-            delay(1000L)
-            Log.d("Coroutines","Canceling..")
-            job1.cancel()
-            job1.join() //will wait till the job is completed and only the log the completed message
-            job3.cancelAndJoin()
-            Log.d("CoroutinesC","Job 1 Cancelled..")
-        }
-
-        runBlocking {
-            delay(2000L)
-            Log.d("Coroutines","Canceling..")
-            mainJob.cancelAndJoin()
-            Log.d("CoroutinesC","MainJob Cancelled..")
-
-        }
 
         view.findViewById<TextView>(R.id.first_text).setOnClickListener {
             findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
